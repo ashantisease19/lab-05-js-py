e3b54145c80cd71d256e6e52a32e1acd7e3b5a43
Ashanti Sease <you@example.com>
2021-03-08 17:42:42 -0500

    both python and javascript madlibs

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+noun = input("Put a noun, a noun is a a word (other than a pronoun) used to identify any of a class of people, places, or things" )
+adjective = input("Put an adjective.")
+adverb = input("Put an adverb.")
+verb = input("Put an verb.")
+
+print("In the buzzing city of New York, " + str(noun))
+print("eats a super " + str(adjective) + " soup in Chinatown.")
+print("After, " + str(noun) +  str(adverb) +  str(verb) + " some hoodlums.")
